6d7f31bbfff964c8a97ad44d73c225b29e89751f
c0der.br1t <c0der.br1t@gmail.com>
2020-01-13 16:16:40 -0600

    Create constructor and begin to set state in App.js. Set up the return in App.js. Create Todo.js code. Write TodoList.js code, prepare for toggle onClick. Add toggleItem method in App.js.  Build out TodoForm.js. Create TodoForm.js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
 import React from 'react';
 
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+const todoItems= [];
+
+/*
+[
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+*/
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state={
+      todoList: todoItems
+    };
+  }
+
+  toggleItem = id => {
+    const newTodoList = this.state.todoList.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          completed: !task.completed
+        };
+      } else {
+        return task;
+      }
+    });
+    this.setState({
+      todoList: newTodoList
+    });
+  };
+
+  addTask = taskName => {
+    const newTask = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todoList: [...this.state.todoList, newTask]
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+        <div className='header'>
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm addTask={this.addTask}/>
+        </div>
+        <TodoList todoItems={this.state.todoList} toggleItem={this.toggleItem} />
+        </div>
     );
   }
 }
