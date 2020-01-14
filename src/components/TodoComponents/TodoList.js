bd9aec3cf322d1aeb401bf0cc4d21331a3a5b0f7
Lambda_School_Loaner_252 <c0der.br1t@gmail.com>
2020-01-14 02:24:16 -0600

    styled app

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -6,13 +6,19 @@ import Todo from './Todo';
 
 const TodoList = props => {
     return (
-        <div className="todo-list">
-            {props.todoItems.map(item => (
-                <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
-            ))}
-            <button className="clear-button" onClick={() => props.clearCompleted()}>
-                Clear Finished Tasks
-            </button>
+        <div >
+            <div className="todo-list">
+                {props.todoItems.map(item => (
+                    <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+                ))}
+            </div>
+            <div className="clear-button-div">
+                <button className="clear-button" onClick={() => props.clearCompleted()}>
+                    Clear Finished Tasks
+                </button>
+            </div>
+
+            
         </div>
     )
 }
